c7d0c12f59610061c4179ec31a276fff629d70f3
AisvarryaaN <aisvarryaan@gmail.com>
2025-01-04 09:24:35 +0530

    Added

--- src/test/java/org/demo/ClassA.java
+++ src/test/java/org/demo/ClassA.java
@@ -2,4 +2,15 @@ package org.demo;
 
 public class ClassA {
 
+	private void method1() {
+		System.out.println("Method 1");
+	}
+
+	private void method2() {
+		System.out.println("Method 2");
+	}
+
+	private void method3() {
+		System.out.println("Method 3");
+	}
 }
